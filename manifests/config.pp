f396735691d82e803860ab000c45e209c38f0d57
Andrew Farler <farlerac@miamioh.edu>
2017-06-22 14:28:04 -0400

    add whitelisting, remove debian specific files

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,14 +1,16 @@
 class amavis::config(
-  $bypass_virus_checks_maps = '0',
-  $bypass_spam_checks_maps  = '0',
   $final_virus_destiny      = 'D_DISCARD',
   $final_banned_destiny     = 'D_BOUNCE',
   $final_spam_destiny       = 'D_DISCARD',
   $final_bad_header_destiny = 'D_BOUNCE',
   $max_servers              = '2',
+  $whitelist                = undef,   # enable whitelisting
+  $whitelist_entries,
 ) {
 
   include amavis
+
+  $real_whitelist = join($whitelist_entries,"','")
 
   file { '/etc/amavisd/amavisd.conf':
     ensure  => present,
@@ -16,4 +18,5 @@ class amavis::config(
     notify  => Service['amavisd'],
     require => Exec['amavis'],
   }
+
 }
