eed291d953253da265ee2a231dad76b517a6e558
Andy Farler <farlerac@miamioh.edu>
2020-04-20 10:14:30 -0400

    add parameters for blacklisting, scanner bypass

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,7 @@
 class amavis::config(
+  $blacklist                = undef,   # enable blacklisting
+  $blacklist_entries,
+  $disable_scanning         = undef,   # bypass all scanning
   $final_virus_destiny      = 'D_DISCARD',
   $final_banned_destiny     = 'D_BOUNCE',
   $final_spam_destiny       = 'D_DISCARD',
@@ -10,6 +13,7 @@ class amavis::config(
 
   include amavis
 
+  $real_blacklist = join($blacklist_entries,"','")
   $real_whitelist = join($whitelist_entries,"','")
 
   file { '/etc/amavisd/amavisd.conf':
